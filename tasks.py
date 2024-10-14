a9dd2d0264f08628ba9b1fe5803478833f7e344a
KnightBlue <domburton@hotmail.co.uk>
2024-10-14 15:35:33 +0100

    Logic update

--- tasks.py
+++ tasks.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import shutil
 import json
 import os
-from os.path import exists
 
 user = ret_auth.user
 key = ret_auth.key
@@ -34,28 +33,17 @@ else:
         f.write(response.content)
     data = response.json()
 
-cover_path = 'static\\images\\cover.png'
-badge_path = 'static\\images\\badge.png'
+game_cover = data[0]['GameIcon']
+cover_response = requests.get(f'https://i.retroachievements.org/{game_cover}', stream=True)
+cover = open(f'{folder_name}\\cover.png','wb')
+shutil.copyfileobj(cover_response.raw, cover)
+cover.close()
 
-cover_exists = exists(cover_path)
-badge_exists = exists(badge_path)
-
-if cover_exists == True:
-    pass
-else:
-    game_cover = data[0]['GameIcon']
-    cover_response = requests.get(f'https://i.retroachievements.org/{game_cover}', stream=True)
-    cover = open(f'{folder_name}\\cover.png','wb')
-    shutil.copyfileobj(cover_response.raw, cover)
-    cover.close()
-if badge_exists == True:   
-    pass
-else: 
-    ach_url = data[0]['BadgeURL']
-    ach_response = requests.get(f'https://i.retroachievements.org/{ach_url}', stream=True)
-    ach = open(f'{folder_name}\\badge.png','wb')
-    shutil.copyfileobj(ach_response.raw, ach)
-    ach.close()
+ach_url = data[0]['BadgeURL']
+ach_response = requests.get(f'https://i.retroachievements.org/{ach_url}', stream=True)
+ach = open(f'{folder_name}\\badge.png','wb')
+shutil.copyfileobj(ach_response.raw, ach)
+ach.close()
     
 game_title = data[0]['GameTitle']
 ach_title = data[0]['Title']
